9fff7144591e02405c45dd6ffd1d0769afd2bfe7
Davide Oggioni (Reti) <davide.oggioni@reti.it>
2020-11-02 10:20:02 +0100

    Modify title

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  navbarTitle = 'Crypto Angular';
+  navbarTitle = 'Crypto';
   constructor() { }
 
   ngOnInit(): void {

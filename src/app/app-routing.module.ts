965f9d81670a33cd48364afe73f52e2232db4edc
Davide Oggioni (Reti) <daviogg@hotmail.it>
2020-11-07 16:49:24 +0100

    complete routing in app.component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AssetDetailComponent } from './asset-detail/asset-detail.component';
 import { AssetsTableComponent } from './assets-table/assets-table.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'Home', pathMatch: 'full' },
-  { path: 'Home', component: AssetsTableComponent, pathMatch: 'full' },
-  { path: '**', redirectTo: 'Home'}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: AssetsTableComponent, pathMatch: 'full' },
+  { path: 'detail', component: AssetDetailComponent, pathMatch: 'full'},
+  { path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({

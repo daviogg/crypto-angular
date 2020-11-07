419126957b044a0e0b9c21f8fa11e8ed999119a6
Davide Oggioni (Reti) <daviogg@hotmail.it>
2020-11-07 16:46:15 +0100

    add home routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AssetsTableComponent } from './assets-table/assets-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: AssetsTableComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: 'Home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
